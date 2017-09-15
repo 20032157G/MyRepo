069cc2c625124276c1fbcd9a06a0e544a4dff4da
Josue Zacarias Olivet Rojas <josuezacarias@gmail.com>
2017-09-15 14:16:37 -0500

    Update al93.py

--- al93.py
+++ al93.py
@@ -9,38 +9,52 @@ class bnlddr():
 	def alg1(self):
 		Y=np.binary_repr(self.y)
 		z=self.x
+		N=self.b
 		for i in range(1,len(Y)):
-			z=pow(z,2)
+			z=pow(z,2)%N
 			if (Y[i] == "1"):
-				z=z*self.x
+				z=z*self.x%N
 		return z
 
 	def alg2(self):
 		Y=np.binary_repr(self.y)
+		N=self.b
 		z=self.x
 		a=1
 		for i in range(0,len(Y)-1):
 			if(Y[i]=="1"):
-				a=z*a
-			z=pow(z,2)
-		return a*z
+				a=z*a%N
+			z=pow(z,2)%N
+		return a*z%N
 
 	def alg3(self):
-		Y=np.base_repr(self.y,self.b)
-		print(Y)
+		B=self.b
+		b=int(math.log(B,2))
+		Y=np.base_repr(self.y,B)
+		#print(Y)
 		z=1
 		for i in range(0,len(Y)):
 			c=0 #np.trunc(np.log(int(Y[i]))/np.log(2))
-			d=int(Y[i])
+			yi=int(Y[len(Y)-1-i])
+			d=int(yi)
 			while (d%2==0)and(d>0):
 				c+=1
 				d/=2
 				#d=int(Y[i])//pow(2,c)
 			z=z*pow(pow(self.x,d),pow(2,c))
 			#print(len(Y),i,c,d,z)
-			if (i<len(Y)-1):
-				z=pow(z,pow(2,self.b))
+			if (i>0):
+				z=pow(z,pow(2,b))
 			#print(z)
 			#print ("###########")
 		return z
 	
+	def alg4(self):
+		b=self.b
+		x=self.x
+		Y=np.base_repr(self.y,b)
+		z=1
+		for i in range(0,len(Y)):
+			yi=int(Y[len(Y)-1-i])
+			z=z*pow(pow(x,yi),pow(b,i))
+		return z
